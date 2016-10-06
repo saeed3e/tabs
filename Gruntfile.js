30579c80f091ad8ba89bd0c1fb90168157301b67
Mohd Saeed <mohd.saeed@Infoedge.com>
2016-10-06 16:30:31 +0530

    udpates for grunt-coverall package ....

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,8 +53,9 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-coveralls');
 
-    grunt.registerTask('test', ['jasmine']);
+    grunt.registerTask('test', ['jasmine',"coveralls"]);
     grunt.registerTask('default', ['uglify','test']);
 
 };
